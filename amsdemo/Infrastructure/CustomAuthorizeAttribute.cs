9eb1c4051e9f5d96f53a0588c0df0711a793993b
Abll <Abll@AAMIR>
2020-01-02 15:55:17 +0500

    Revert "Revert "Add project files.""
    This reverts commit 033dcd44724ae903fbc6c14e61751a11305f3c95.

---
+++ amsdemo/Infrastructure/CustomAuthorizeAttribute.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using amsdemo.Models;
+
+namespace amsdemo.Infrastructure
+{
+    public class CustomAuthorizeAttribute : AuthorizeAttribute
+    {
+        private readonly string[] allowedroles;
+
+        public CustomAuthorizeAttribute(params string[] roles)
+        {
+            this.allowedroles = roles;
+        }
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            bool authorize = false;
+            var userName = Convert.ToString(httpContext.Session["UserName"]);
+            if(!string.IsNullOrEmpty(userName))
+                using(var context = new SqlContext())
+                {
+                    var userRole = (from u in context.tblUsers
+                                    join r in context.tblRoles on u.RoleId equals r.Id
+                                    join a in context.tblAdminchecks on u.AdminId equals a.AdminId
+                                    where u.UserName == userName
+                                    select new
+                                    {
+                                        r.RoleName,
+                                        a.IsAdmin                                       
+                                    }).FirstOrDefault();
+                    foreach(var role in allowedroles)
+                    {
+                        if (role == userRole.RoleName||role == userRole.IsAdmin) return true;
+                    }
+                                    
+                }
+            return authorize;           
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectToRouteResult(
+                new RouteValueDictionary
+                {
+                    {"controller", "Home" },
+                    {"action", "UnAuthorized" }
+                });
+                
+        }
+
+    }
+}
---
+++ amsdemo/Migrations/201912261408371_Initial.cs
@@ -0,0 +1,55 @@
+﻿namespace amsdemo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+               "dbo.Roles",
+               c => new
+               {
+                   Id = c.Int(nullable: false, identity: true),
+                   Name = c.String(),
+               })
+               .PrimaryKey(t => t.Id);
+
+            CreateTable(
+                "dbo.Users",
+                c => new
+                {
+                    UserId = c.Int(nullable: false, identity: true),
+                    UserName = c.String(),
+                    Password = c.String(),
+                    RoleId = c.Int(nullable: false),
+                    isAdmin = c.Boolean(nullable: false),
+
+                })
+                .PrimaryKey(t => t.UserId);
+
+            CreateTable(
+                "dbo.Departments",
+                c => new
+                {
+                    DepartmentId = c.Int(nullable: false, identity: true),
+                    DepartmentName = c.String(),
+
+
+                })
+                .PrimaryKey(t => t.DepartmentId);
+
+
+
+        }
+
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+            DropTable("dbo.Roles");
+            DropTable("dbo.Departments");
+
+        }
+    }
+}
